f377454c5ddaa0e0e30c2fe1948151c66f7304d6
0Strategist0 <0strategist0@gmail.com>
2022-07-16 22:37:28 -0700

    Burning actually works now
    Burning essentially done, just want to make an explosion effect

--- project/script/pot.gd
+++ project/script/pot.gd
@@ -3,6 +3,8 @@ extends RigidBody
 export var interaction_range : float = 20.0
 
 const SMOKE_SCENE := preload("res://prefabs/effects/smoke.tscn")
+const DIE_UI := preload("res://prefabs/DieUI.tscn")
+const BURN_IMG := preload("res://art/dice-fire.png")
 const DIE_COOK_TIME := 8.0
 const BURN_TIME := 10.0
 
@@ -20,6 +22,7 @@ var cooking := false
 var held := false
 var dumping := false
 var cooking_time := 0.0
+var burnt := false
 
 func _ready():
 	var smoke_scene := SMOKE_SCENE.instance()
@@ -31,6 +34,20 @@ func _ready():
 	player1.connect("interact", self, "_on_player_interact")
 
 func _process(delta):
+	if cooking_time >= numbers.size() * DIE_COOK_TIME + BURN_TIME:
+		burnt = true
+		numbers.clear()
+		
+		UI.size_multiplier = 2.0
+		UI.clear_dice()
+		UI.margin_top = -UI.image_size * UI.size_multiplier
+		var burn_img := DIE_UI.instance()
+		burn_img.texture = BURN_IMG
+		UI.add_child(burn_img)
+	
+	if burnt:
+		cooking_progress.value = numbers.size() * DIE_COOK_TIME + BURN_TIME
+	
 	var fraction_to_burn := clamp(range_lerp(cooking_time, cooking_progress.max_value, BURN_TIME + cooking_progress.max_value, 1.0, 0.0), 0.0, 1.0)
 	
 	smoke.color = Color(fraction_to_burn, fraction_to_burn, fraction_to_burn)
@@ -40,7 +57,7 @@ func _process(delta):
 		cooking_time += delta
 
 func _on_player_interact(player: KinematicBody, held_object: Spatial):
-	if not held and held_object is Die and to_local(player.global_transform.origin).length_squared() < interaction_range:
+	if not held and not burnt and held_object is Die and to_local(player.global_transform.origin).length_squared() < interaction_range:
 		
 		# Make sure I'm the closest that can eat a die
 		var player_position := player.global_transform.origin
@@ -99,6 +116,9 @@ func garbage(player: KinematicBody):
 	numbers.clear()
 	UI.clear_dice()
 	cooking_time = 0.0
+	burnt = false
+	UI.size_multiplier = 1.0
+	UI.clear_dice()
 	print(numbers)
 	yield(animation, "animation_finished")
 	dumping = false
