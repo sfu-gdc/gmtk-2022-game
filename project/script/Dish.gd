58ce651908955a0d29d71483a98c5977e8d198df
0Strategist0 <0strategist0@gmail.com>
2022-07-17 04:15:23 -0700

    Dish submission done
    Dish done

--- project/script/Dish.gd
+++ project/script/Dish.gd
@@ -4,7 +4,11 @@ const DISH_LAYER := 1
 
 onready var level := $"/root".get_child($"/root".get_child_count() - 1)
 
-var number : int
+var number : int setget set_number
+
+func set_number(value: int):
+	number = value
+	$DishLabel.text = str(number)
 
 func _init():
 	var mesh = MeshInstance.new()
@@ -18,9 +22,6 @@ func _init():
 	mesh.mesh.material.uv1_scale = Vector3(1, 1, 1)
 	add_child(mesh)
 	mesh.translation += 0.75 * Vector3.UP
-
-func _ready():
-	$DishLabel.text = number
 
 func pickup(player: KinematicBody) -> RigidBody:
 	# Attach to player and disable collisions
@@ -42,7 +43,7 @@ func place(player: KinematicBody) -> bool:
 		player.remove_child(self)
 		level.add_child(self)
 		global_transform = save_transform
-		global_transform.origin = (global_transform.origin - 1.5 * player.global_transform.basis.z).snapped(Vector3(2.0, 2.0, 2.0))
+		global_transform.origin = (global_transform.origin - 2.0 * player.global_transform.basis.z)
 	else:
 		global_transform = save_transform
 		global_transform.origin = global_transform.origin.snapped(Vector3(2.0, 2.0, 2.0))
