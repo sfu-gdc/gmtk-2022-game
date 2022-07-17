05337a66392e2a08886d86019f7924a814f2bec6
Andrew <andrewliang0123@gmail.com>
2022-07-17 02:34:36 -0700

    addition check to collider

--- project/customClasses/Die.gd
+++ project/customClasses/Die.gd
@@ -18,6 +18,8 @@ var throwable = true
 # throwing status will keep releasing signal
 var throwing = false
 
+var collide_count = 0
+
 onready var dice_tex_1 = load("res://art/white-die.png")
 
 # for stopping & starting particles
@@ -140,11 +142,14 @@ func _process(_delta):
 func _on_body_entered(body:Node):
 	#if die collised to "attachable" object
 	if not "attachable" in body:
+		collide_count += 1
+		if collide_count > 5:
+			throwing = false
+			collide_count = 0
 		return
 	if throwing:
 		body._on_throwable_interact(self)
 		queue_free()
-		
 
 	
 func pickup(player: KinematicBody) -> Die:
