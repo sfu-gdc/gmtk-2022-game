16c7982bcc7211fe84a9bd25032a2febfc442a6f
Andrew <andrewliang0123@gmail.com>
2022-07-17 11:15:11 -0700

    Update boat_animation.gd

--- project/script/boat_animation.gd
+++ project/script/boat_animation.gd
@@ -7,6 +7,7 @@ var going_out = false
 var going_in = false
 
 onready var animationTree : AnimationTree = $AnimationTree
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer2
 onready var animationState
 
 # Called when the node enters the scene tree for the first time.
@@ -15,10 +16,10 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	if going_out:
-		#print("Boat is going !!!!!")
+	if going_out && animationPlayer.current_animation == "Idle":
 		animationState.travel("go_out")
 		going_out = false
+		
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
